13c88d521838699d6c1d32a29b8b27b94ac2b680
lnielsen <louiseenielsen@gmail.com>
2017-01-25 17:14:55 -0500

    did ex02

--- exercises/ex02/stack.c
+++ exercises/ex02/stack.c
@@ -11,36 +11,50 @@ License: GNU GPLv3
 #define SIZE 5
 
 int *foo() {
+    // in local scope, instantiate the counter and the array length 5
     int i;
     int array[SIZE];
 
-    //printf("%p\n", array);
+    // print the location of the array
+    printf("%p\n", array);
 
+    // write 42 as every entry in the array
     for (i=0; i<SIZE; i++) {
-	array[i] = 42;
+  array[i] = 42;
     }
+    // return the location of the array
     return array;
 }
 
 void bar() {
+    // in local scope, instantiate the counter and the array length 5
     int i;
     int array[SIZE];
 
-    //printf("%p\n", array);
+    // print the location of the array
+    printf("%p\n", array);
 
+    // write 0, 1, 2, 3, 4 as entries in the array
     for (i=0; i<SIZE; i++) {
-	array[i] = i;
+  array[i] = i;
     }
+    // return nothing
 }
 
 int main()
 {
+    // instantiate a counter
     int i;
+    // set a pointer to the location of the array returned by foo()
+    // this doesn't work because the array is stack allocated
     int *array = foo();
+    // run bar()
     bar();
 
+    // print the array
+    // this doesn't work because you can't access the array
     for (i=0; i<SIZE; i++) {
-	printf("%d\n", array[i]);
+  printf("%d\n", array[i]);
     }
 
     return 0;
